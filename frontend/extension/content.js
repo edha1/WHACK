e32c316090d1c6ea181ac4a115f9d43ad34b5948
edha1 <edha.misra@gmail.com>
2025-10-18 17:27:24 +0100

    feat: can get title, content and date info from news page

--- frontend/extension/content.js
+++ frontend/extension/content.js
@@ -1,9 +1,33 @@
-
 // send message to content.js so it can interact with the webpage
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+    let h1Texts = ""; 
+    let dates = ""; 
+    let content = []; 
+    const articles = document.getElementsByTagName("article"); 
+    const DATE_REGEX = /\b([1-9]|[12][0-9]|3[01])(st|nd|rd|th)?\s+(January|February|March|April|May|June|July|August|September|October|November|December|Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{4}\b/g;
+
+    // getting the title of the data
     if (message.action === "getH1") {
-        const h1Elements = document.getElementsByTagName("h1");
-        const h1Texts = Array.from(h1Elements).map(h1 => h1.innerText.trim());
-        sendResponse({ h1s: h1Texts });
+        for (let article of articles) {
+            const h1Elements = article.querySelectorAll("h1");
+            h1Texts = Array.from(h1Elements).map(h1 => h1.innerText.trim());
+        }
+
+    // get the date of the data
+    } else if (message.action === "getDate") {
+        const dateFromTimeEl = document.getElementsByTagName("time"); // date might not be under the time tag for all websites 
+        let datesUnfiltered = ""; 
+        if (dateFromTimeEl.length > 0) {
+            datesUnfiltered = Array.from(dateFromTimeEl).map(date => date.innerText.trim()); 
+            dates = datesUnfiltered.filter(date => DATE_REGEX.test(date));
+        }
+    } else if (message.action === "getContent") { 
+        for (let article of articles) {
+            const allContent = article.querySelectorAll("p");
+            content.push(...Array.from(allContent).map(p => p.innerText.trim()));
+        }
     }
+
+    sendResponse({ h1: h1Texts, date: dates, content: content});
 });
--- frontend/extension/popup.js
+++ frontend/extension/popup.js
@@ -1,17 +1,33 @@
 let btn = document.getElementById("get-input"); 
-    btn.addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const tabId = tabs[0].id; // get current tab 
-    
-    // inject content.js 
+let header = ""; 
+let date = ""; 
+let content = ""; 
+
+btn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tabId = tabs[0].id;
+
     chrome.scripting.executeScript({ target: { tabId }, files: ["content.js"] }, () => {
-        chrome.tabs.sendMessage(tabId, { action: "getH1" }, (response) => {
-        console.log(response);
+      
+      // get title
+      chrome.tabs.sendMessage(tabId, { action: "getH1" }, (response1) => {
+        document.getElementById("header").innerText =
+          response1?.h1?.join("\n") || "No <h1> found.";
+      });
 
-        // test title text 
-        document.getElementById("output").innerText = response?.h1s?.join("\n") || "No <h1> found.";
-        });
-    });
-    });
+      // get date
+      chrome.tabs.sendMessage(tabId, { action: "getDate" }, (response2) => {
+        document.getElementById("date").innerText =
+          response2?.date?.join("\n") || "No <time> found.";
+      });
 
+      // get content
+      chrome.tabs.sendMessage(tabId, { action: "getContent" }, (response3) => {
+        document.getElementById("content").innerText =
+          response3?.content?.join("\n") || "No <p> found.";
+      });
+
+    });
+  });
 });
+
